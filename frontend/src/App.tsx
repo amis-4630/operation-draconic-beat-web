bc6e883280852b6436f27c905778cadcef610b6e
Chad Thomas <chad.thomas@neudesic.com>
2023-03-17 14:36:59 -0400

    merge updates from `main`

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,108 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const openMenu = () => {
+    document.querySelector(".sidebar")?.classList.add("open");
+  }
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar")?.classList.remove("open");
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+
+      <head>
+        <link rel="stylesheet" href="style.css" />
+        <title>Operation: Draconic Beat</title>
+      </head>
+
+      <body>
+        <div>
+          <header className="header">
+            <div className="brand">
+              <button onClick={openMenu}>&#9776;</button>
+              <a href="index.html">Operation: Draconic Beat</a>
+            </div>
+            <div className="header-links">
+              <a href="cart.html">Cart</a>
+              <a href="login.html">Login</a>
+            </div>
+          </header>
+          <aside className="sidebar">
+            <h3>Shopping Categories</h3>
+            <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+            <ul>
+              <li>
+                <a href="index.html">All Products</a>
+              </li>
+              <li>
+                <a href="index.html">Shirts</a>
+              </li>
+              <li>
+                <a href="index.html">Pants</a>
+              </li>
+              <li>
+                <a href="index.html">Shoes</a>
+              </li>
+            </ul>
+          </aside>
+          <main className="main">
+            <ul className="products">
+              <li>
+                <div className="product">
+                  <img className="product-image" src="images/d1.jpg" alt="d1 " />
+                  <div className="product-name">
+                    <a href="product.html">d1 Shirt</a>
+                  </div>
+                  <div className="product-brand">d1 </div>
+                  <div className="product-price">$60</div>
+                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                </div>
+              </li>
+              <li>
+                <div className="product">
+                  <img className="product-image" src="images/d1.jpg" alt="d1 " />
+                  <div className="product-name">
+                    <a href="product.html">d1 Shirt</a>
+                  </div>
+                  <div className="product-brand">d1 </div>
+                  <div className="product-price">$60</div>
+                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                </div>
+              </li>
+              <li>
+                <div className="product">
+                  <img className="product-image" src="images/d1.jpg" alt="d1 " />
+                  <div className="product-name">
+                    <a href="product.html">d1 Shirt</a>
+                  </div>
+                  <div className="product-brand">d1 </div>
+                  <div className="product-price">$60</div>
+                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                </div>
+              </li>
+              <li>
+                <div className="product">
+                  <img className="product-image" src="images/d1.jpg" alt="d1 " />
+                  <div className="product-name">
+                    <a href="product.html">d1 Shirt</a>
+                  </div>
+                  <div className="product-brand">d1 </div>
+                  <div className="product-price">$60</div>
+                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
+                </div>
+              </li>
+            </ul>
+          </main>
+          <footer className="footer">
+            &copy; 2023 Operation: Draconic Beat
+          </footer>
+        </div>
+      </body>
     </div>
   );
 }
