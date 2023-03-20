d4e6185ca26a5615bb9fbd2429f84cec58d9d501
Chad Thomas <chad.thomas@neudesic.com>
2023-03-20 09:33:54 -0400

    finish navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Home/Home';
+import Products from './Products/Products';
+
 
 function App() {
 
@@ -13,97 +16,51 @@ function App() {
 
 
   return (
-    <div className="grid-container">
-
-      <head>
-        <link rel="stylesheet" href="style.css" />
-        <title>Operation: Draconic Beat</title>
-      </head>
-
-      <body>
-        <div>
-          <header className="header">
-            <div className="brand">
-              <button onClick={openMenu}>&#9776;</button>
-              <a href="index.html">Operation: Draconic Beat</a>
-            </div>
-            <div className="header-links">
-              <a href="cart.html">Cart</a>
-              <a href="login.html">Login</a>
-            </div>
-          </header>
-          <aside className="sidebar">
-            <h3>Shopping Categories</h3>
-            <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-            <ul>
-              <li>
-                <a href="index.html">All Products</a>
-              </li>
-              <li>
-                <a href="index.html">Shirts</a>
-              </li>
-              <li>
-                <a href="index.html">Pants</a>
-              </li>
-              <li>
-                <a href="index.html">Shoes</a>
-              </li>
-            </ul>
-          </aside>
-          <main className="main">
-            <ul className="products">
-              <li>
-                <div className="product">
-                  <img className="product-image" src="images/d1.jpg" alt="d1 " />
-                  <div className="product-name">
-                    <a href="product.html">d1 Shirt</a>
-                  </div>
-                  <div className="product-brand">d1 </div>
-                  <div className="product-price">$60</div>
-                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                </div>
-              </li>
-              <li>
-                <div className="product">
-                  <img className="product-image" src="images/d1.jpg" alt="d1 " />
-                  <div className="product-name">
-                    <a href="product.html">d1 Shirt</a>
-                  </div>
-                  <div className="product-brand">d1 </div>
-                  <div className="product-price">$60</div>
-                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                </div>
-              </li>
-              <li>
-                <div className="product">
-                  <img className="product-image" src="images/d1.jpg" alt="d1 " />
-                  <div className="product-name">
-                    <a href="product.html">d1 Shirt</a>
-                  </div>
-                  <div className="product-brand">d1 </div>
-                  <div className="product-price">$60</div>
-                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                </div>
-              </li>
-              <li>
-                <div className="product">
-                  <img className="product-image" src="images/d1.jpg" alt="d1 " />
-                  <div className="product-name">
-                    <a href="product.html">d1 Shirt</a>
-                  </div>
-                  <div className="product-brand">d1 </div>
-                  <div className="product-price">$60</div>
-                  <div className="product-rating">4.5 Stars (10 Reviews)</div>
-                </div>
-              </li>
-            </ul>
-          </main>
-          <footer className="footer">
-            &copy; 2023 Operation: Draconic Beat
-          </footer>
-        </div>
-      </body>
-    </div>
+    <Router>
+      <div className="grid-container">
+        <body>
+          <div>
+            <header className="header">
+              <div className="brand">
+                <button onClick={openMenu}>&#9776;</button>
+                <a href="index.html">Operation: Draconic Beat</a>
+              </div>
+              <div className="header-links">
+                <Link to="/">Home</Link>
+                <Link to="/catalog">Catalog</Link>
+              </div>
+            </header>
+            <aside className="sidebar">
+              <h3>Shopping Categories</h3>
+              <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+              <ul>
+                <li>
+                  <a href="index.html">All Products</a>
+                </li>
+                <li>
+                  <a href="index.html">Shirts</a>
+                </li>
+                <li>
+                  <a href="index.html">Pants</a>
+                </li>
+                <li>
+                  <a href="index.html">Shoes</a>
+                </li>
+              </ul>
+            </aside>
+            <main className="main">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/catalog" element={<Products />} />
+              </Routes>
+            </main>
+            <footer className="footer">
+              &copy; 2023 Operation: Draconic Beat
+            </footer>
+          </div>
+        </body>
+      </div>
+    </Router>
   );
 }
 
